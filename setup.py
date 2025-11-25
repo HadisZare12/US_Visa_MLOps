82a75da70075e92f29c8992fc79bf3fb56e5b0a0
hadis <hadiszare405@gmail.com>
2025-11-25 22:19:44 +0100

    requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='us_visa',
+    version='0.0.0',
+    author = 'Hadis',
+    author_email='Profi.hadiszare@gmail.com',
+    packages=find_packages()
+)
